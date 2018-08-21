142a8560856b2e702c5bce27556d8b70e7db48ad
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-08-21 12:43:49 -0700

    Created attribute readers and tested with a sample 'Earth' class

--- planet.rb
+++ planet.rb
@@ -1,4 +1,17 @@
 class Planet
+  # Setting attributes to be readable
+  attr_reader :name, :color, :mass_kg, :distance_from_the_sun_km, :fun_fact
 
+  # Adding Constructor
+  def initialize(name, color, mass_kg, distance_from_the_sun_km, fun_fact)
+    @name = name
+    @color = color
+    @mass_kg = mass_kg
+    @distance_from_the_sun_km = distance_from_the_sun_km
+    @fun_fact = fun_fact
+  end
 
-end 
+end
+
+earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
+p earth.name
