c968954bc33f97be2545d2c810ffac1b78937069
Katricia Smith <smithkatricia@gmail.com>
2018-08-21 11:10:13 -0700

    Saved each parameter of constructor class in instance variable

--- planet.rb
+++ planet.rb
@@ -2,5 +2,9 @@ class Planet
 
 # Add a constructor to your Planet class.
   def initialize(name, color, mass_kg, distance_from_sun_km, fun_fact)
-
+    @name = name
+    @color = color
+    @mass_kg = mass_kg
+    @distance_from_sun_km = distance_from_sun_km
+    @fun_fact = fun_fact
 end
