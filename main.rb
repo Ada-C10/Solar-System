e7d59f03a4f50ff936e7bf1fa799572ec5d08191
Katricia Smith <smithkatricia@gmail.com>
2018-08-21 15:32:41 -0700

    Added Planet instances into main method

--- main.rb
+++ main.rb
@@ -14,16 +14,21 @@ def main
   puts mercury.summary
 
   puts venus.summary
+
+  solar_system = SolarSystem.new('Sol')
+
+  earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
+  solar_system.add_planet(earth)
+
+  mercury = Planet.new("Mercury", "grey", 3.3011e23, 5.79e7, "is named after a Roman God")
+  solar_system.add_planet(mercury)
+
+  venus = Planet.new("Venus", "yellowish-white", 4.87e24, 1.082e8, "may have had oceans in the past")
+  solar_system.add_planet(venus)
+
+
+  list = solar_system.list_planets
+  puts list
 end
 
 main
-
-
-
-solar_system = SolarSystem.new('Sol')
-
-earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
-solar_system.add_planet(earth)
-
-list = solar_system.list_planets
-puts list
