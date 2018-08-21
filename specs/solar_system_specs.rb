c52e05a19a95003ba0da969e09b20c2d9efb700d
Amanda Ungco <amanda.ungco@gmail.com>
2018-08-21 16:32:25 -0700

    drafted spec file

---
+++ specs/solar_system_specs.rb
@@ -0,0 +1,12 @@
+require 'minitest'
+require_relative '../lib/solar_system.rb'
+require_relative '../lib/planets.rb'
+require_relative '../lib/main.rb'
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+describe 'Check SolarSystem Methods' do
+  #Arrange & act
+  #assert
+  #
+#create nominal and edge cases
+#test distance between method if first planet is closer than second planet
