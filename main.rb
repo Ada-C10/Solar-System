31ad68fde2518f457a890aed3c60cd2335154274
Jane <jane.edwards.mcnear@gmail.com>
2018-08-21 14:44:09 -0700

    Complted Wave 2

--- main.rb
+++ main.rb
@@ -1,15 +1,32 @@
 require_relative 'planet'
+require_relative 'solar_system'
 
 def main
-  neptune = Planet.new('Neptune', 'blue', 1050.9318, 2.793e10, 'farthest known planet from the Sun in the Solar System')
-  puts neptune.fun_fact
+  neptune = Planet.new('Neptune', 'blue', 1050.9318, 4498252900, 'farthest known planet from the Sun in the Solar System')
   puts neptune.color
   puts neptune.summary
 
   saturn = Planet.new('Saturn', 'yellowish-brown', 5.6834e26, 1433449370, 'famous for rings')
-  puts saturn.fun_fact
   puts saturn.color
   puts saturn.summary
+
+  solar_system = SolarSystem.new('Sol')
+
+  earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
+
+  solar_system.add_planet(neptune)
+  solar_system.add_planet(saturn)
+  solar_system.add_planet(earth)
+
+  list = solar_system.list_planets
+  puts list
+
+  found_planet = solar_system.find_planet_by_name('Earth')
+  # puts found_planet
+  # puts found_planet.summary
+
+  dist_between = solar_system.distance_between(neptune, saturn)
+  puts dist_between
 end
 
 main
--- solar_system.rb
+++ solar_system.rb
@@ -1,26 +1,51 @@
+require 'pry'
+
 class SolarSystem
 
   def initialize(star_name)
     @star_name = star_name
+    @planets = []
   end
+  # attr_writer :planets
+  attr_reader :star_name
 
-  attr_reader :star_name :planets
-
-  # def planets
-  #   return @planets = []
-  # end
-  @planets = []
 
   def add_planet(planet)
     @planets << planet
   end
 
   def list_planets
-
     list = @planets.map.with_index do |planet, index|
-      "#{index + 1}. #{planet}"
+      "#{index + 1}. #{planet.name}\n"
     end
-    return list
+    return "Planets orbiting #{star_name}\n" + list.join
   end
+
+  def find_planet_by_name(planet_string)
+    found_planets = []
+    @planets.each do |planet|
+      if planet_string.capitalize == planet.name
+        found_planets << planet
+      else
+        "There is no such planet in this solar system"
+      end
+    end
+    if found_planets.length > 1
+      puts "We found multiple planets. #{found_planets}"
+      return found_planets
+    else
+      found_planets.reduce
+    end
+
+    def distance_between(planet_one, planet_two)
+      if planet_one.distance_from_sun_km > planet_two.distance_from_sun_km
+        return planet_one.distance_from_sun_km - planet_two.distance_from_sun_km.round
+      else planet_two.distance_from_sun_km > planet_one.distance_from_sun_km
+        return planet_two.distance_from_sun_km - planet_one.distance_from_sun_km
+      end
+    end
+
+  end
+
 
 end
