3c0a0932ca5f9a23a05a6caf1e380eb7a1ea3315
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-08-21 14:11:13 -0700

    Added method to list planets

--- lib/main.rb
+++ lib/main.rb
@@ -1,4 +1,5 @@
 require_relative 'planet'
+require_relative 'solar_system'
 
 def main
   # Create an instance of planet
@@ -6,6 +7,13 @@ def main
   # Create another instance of planet
   venus = earth = Planet.new('Venus', 'white', 5.972e24, 1.08e8, 'Is the second brightest natural object in the sky')
 
+  # Create an instance of SolarSystem
+  solar_system = SolarSystem.new('Sol')
+  solar_system.add_planet(venus)
+  solar_system.add_planet(mars)
+
+  list = solar_system.list_planets
+  puts list
   puts mars.summary
   puts venus.summary
 
--- lib/solar_system.rb
+++ lib/solar_system.rb
@@ -1,12 +1,37 @@
 class SolarSystem
 
-attr_reader :star_name, :planets
+  attr_reader :star_name, :planets
 
 
-def initialize(star_name)
-  @planets = []
-  @star_name = star_name
-end
+  def initialize(star_name)
+    # Starting planets array as empty
+    @planets = []
+    @star_name = star_name
+  end
 
+  def add_planet(planet)
+    # PUSHING planet to planets array
+    @planets << planet
+  end
+
+  def list_planets
+    # Setting iterator with start value 1
+    i = 1
+    # Adding first line of string to planet_lsit
+    planet_list = "Planets orbiting #{@star_name}:\n"
+    # Adding to planet_list via looping through planets array
+    @planets.each do |planet|
+      # Adding each planet to message
+      planet_list +="#{i}.#{planet.name}\n"
+      # Increasing iterator
+      i += 1
+    end
+    return planet_list
+  end
+
+  def find_planet_by_name(name)
+
+
+  end
 
 end
