833777f35949090fa7631a822262364ad4498f34
Katrina Aganon <katrina.aganon@gmail.com>
2018-08-21 12:01:53 -0700

    Used summary method in my main method and printed each instance of the planet's description

--- main.rb
+++ main.rb
@@ -3,13 +3,10 @@ require_relative 'planet'
 def main
   mars = Planet.new("Mars","reddish-brown", 6.39e23, 2.279e8, "Named after a mythological figure - the Roman god of war"  )
   jupiter = Planet.new("Jupiter", "shades of white, brown, and yellow", 1.898e27, 7.785e8, "It does not have a true surface, as the planet is mostly swirling gases and liquids")
-  puts mars.name.upcase
-  puts mars.fun_fact
+  puts mars.summary
   puts
-  puts jupiter.name.upcase
-  puts jupiter.fun_fact
+  puts jupiter.summary
   puts
 end
-
 
 main
