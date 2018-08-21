de6361351a4a74fbe5fb325e43c017a9e6712988
Amber Lynn <griffiam@gmail.com>
2018-08-21 11:57:51 -0700

    updated summary method

---
+++ planet.rb
@@ -0,0 +1,17 @@
+class Planet
+  attr_reader :name, :color, :mass_kg, :distance_from_sun_km, :fun_fact
+
+  def initialize(name, color, mass_kg, distance_from_sun_km, fun_fact)
+    @name = name
+    @color = color
+    @mass = mass_kg
+    @distance = distance_from_sun_km
+    @fun_fact = fun_fact
+  end
+
+  def summary
+    return "#{@name}: \nColor: #{@color} \nMass in kg: #{@mass} \nDistance from sun in kg: #{@distance} \nFun Fact about #{@name}: #{@name} is #{@fun_fact}."
+  end
+end
+
+earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
