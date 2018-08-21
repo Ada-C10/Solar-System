a13935a721a8a86a4d63b904035613e538ff860a
Melissa O'Hearn <melissa.ohearn@gmailcom>
2018-08-21 16:17:07 -0700

    added Mars, added found_planet to invoke find planet by name method

--- planet.rb
+++ planet.rb
@@ -2,9 +2,9 @@ require 'pry'
 
 class Planet
 
-# construstor
+# helper method
   attr_reader :name, :color, :mass_kg, :distance_from_sun_km, :fun_fact
-
+# construstor
   def initialize(name, color, mass_kg, distance_from_sun_km, fun_fact)
 
     @name = name
@@ -13,8 +13,8 @@ class Planet
     @distance_from_sun_km = distance_from_sun_km
     @fun_fact = fun_fact
   end
-
+#instance method
   def summary
-    return "#{@name} is #{@color} and weighs #{@mass_kg} and is #{distance_from_sun_km} km away from the sun. A fun fact about #{@name}is: #{fun_fact}"
+    return "#{@name} is #{@color} and weighs #{@mass_kg} and is #{distance_from_sun_km} km away from the sun. A fun fact about #{@name} is: #{fun_fact}"
   end
 end
