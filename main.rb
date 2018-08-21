550665fe2e43102b5e32137b87e43e134a00a6f7
Carly Reiter <yepodacarly@gmail.com>
2018-08-21 14:56:59 -0700

    Completed Wave 2

--- main.rb
+++ main.rb
@@ -11,7 +11,10 @@ def main
 
   mars = Planet.new("Mars", "orange", 6.39e23, 2.279e8, "Suspected to have traces of water")
   solar_system.add_planet(mars)
-  
+
+  jupiter = Planet.new("Jupiter", "white, red, orange, brown, and yellow", 1.898e27, 7.785e8, "Has rings")
+  solar_system.add_planet(jupiter)
+
   # puts earth.name #Earth
   # puts earth.fun_fact #Only known planet to support life
   # puts earth.color #blue-green
@@ -23,10 +26,16 @@ def main
 
   # puts earth.summary
   # puts mars.summary
-
+  # puts jupiter.summary
+  puts "Planets orbiting #{solar_system.star_name}:"
   list = solar_system.list_planets
-
   puts list
+
+  found_planet = solar_system.find_planet_by_name("Earth")
+  puts found_planet
+  puts found_planet.name
+  puts found_planet.summary
+
 
 
 end
--- solar_system.rb
+++ solar_system.rb
@@ -3,22 +3,34 @@ class SolarSystem
   attr_reader :star_name, :planets
 
   def initialize(star_name)
-    @star_name = star_name
+    @star_name = star_name #Sol
     @planets = []
   end
 
+# Placing each of the planets to an array of planets (@planets) in this solar system (Sol)
   def add_planet (planet)
     return @planets << planet
   end
 
+# Creating strings with each planet name and their index (+1) to create the numbered list.
   def list_planets
     list = []
     @planets.each_with_index do |planet, index|
-      list << "#{index + 1}: #{planet.name}"
+      list << "#{index + 1}. #{planet.name}"
     end
+    # pushing the top explanation into the array, so it'll print out as the question asks.
+    # list.push "Planets orbiting #{@star_name}:"
+    # list.insert(0, list.delete_at(2))
     return list
-
   end
 
+  def find_planet_by_name(planet) #"Earth"
+    @planets.each do |planets| #@planet = [earth, mars, jupiter]
+      if planet == planets.name.capitalize
+        found_planet = planets
+      end
+      return found_planet
 
+    end
+  end
 end
