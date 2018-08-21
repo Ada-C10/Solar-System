e763ea6ae5bc23e110d5a2d4583404e62cb0ee9e
Karis Kim <ouniiq@hotmail.com>
2018-08-21 16:13:50 -0700

    prints string that lists all planets in the given solar system

--- solar_system.rb
+++ solar_system.rb
@@ -1,4 +1,5 @@
 require_relative 'planet'
+require 'pry'
 
 class SolarSystem
   attr_reader :planets, :star_name
@@ -8,7 +9,16 @@ class SolarSystem
   end
 
   def list_planets
+    list_of_planets = "Planets orbiting #{star_name}\n"
+    
+    count = 1
+    @planets.each do |planet|
+      new_planet = "#{count}. #{planet.name}\n"
+      list_of_planets << new_planet
+      count += 1
+    end
 
+    return list_of_planets
   end
 
   def initialize(star_name)
