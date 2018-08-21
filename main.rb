5e54a337143f24a965ebcd99b33e23ef3228b85f
Goeun Park <goeuntothepark@gmail.com>
2018-08-21 14:38:52 -0700

    completed wave 2 requirements

--- main.rb
+++ main.rb
@@ -1,9 +1,25 @@
-require_relative 'planet.rb'
+require 'pry'
+require_relative 'planet'
+require_relative 'solar_system'
 
 def main
   earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
 
   saturn = Planet.new('Saturn', 'yellow-brown', 5.683e26, 1.433e9, 'Has 150 moons and smaller moonlets')
+
+  solar_system = SolarSystem.new('Sun')
+
+  solar_system.add_planet(earth)
+  solar_system.add_planet(saturn)
+
+  list = solar_system.list_planets
+
+  found_planet = solar_system.find_planet_by_name('earth')
+
+  puts list
+  puts found_planet
+  puts found_planet.summary
+
 
 end
 
