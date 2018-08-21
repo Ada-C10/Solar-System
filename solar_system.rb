75a8fe7e307b944c295298425aa8a65fea5fb360
BarbaraWidjono <barbarawidjono@gmail.com>
2018-08-21 14:12:14 -0700

    Changed implementation of instance method add_planet

--- solar_system.rb
+++ solar_system.rb
@@ -7,12 +7,13 @@ class SolarSystem
 
   def initialize(star_name)
     @star_name = star_name
+    # This is an array of planet objects
     @planets = []
   end
 
 
   def add_planet(instance_of_Planet)
-    @planets << instance_of_Planet.name
+    @planets << instance_of_Planet
   end
 
 
@@ -21,11 +22,22 @@ class SolarSystem
 
     index = 0
     while index < @planets.length
-      list_of_planets << "#{index + 1}. #{@planets[index]}\n"
+      list_of_planets << "#{index + 1}. #{@planets[index].name}\n"
     index += 1
     end
 
     return list_of_planets
   end
 
+
+  def find_planet_by_name(planet_name)
+    puts "In the find planet"
+    # puts @planets
+    @planets.each do |object|
+      if object.name == planet_name
+        return object
+      end
+    end
+
+  end
 end
