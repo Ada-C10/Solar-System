8d3ad77b882f764faead088129ac3a0fdd694057
Anibel America <ameriabel@gmail.com>
2018-08-21 15:37:15 -0700

    Adds a 4th option for CLI called add planet

--- main.rb
+++ main.rb
@@ -15,6 +15,25 @@ def planet_details(solar_system)
 
 end
 
+def add_planet_option(solar_system)
+  puts "Please provide some details for the planet you'd like to add."
+
+  print "Planet name: "
+  name = gets.chomp
+  print "Color: "
+  color = gets.chomp
+  print "Mass (kg): "
+  mass_kg = gets.chomp.to_f # TODO: add error handling
+  print "Distance from the sun (km): "
+  distance_from_sun_km = gets.chomp.to_f # TODO: add error handling
+  print "Fun Fact: "
+  fun_fact = gets.chomp
+
+  new_planet = Planet.new(name, color, mass_kg, distance_from_sun_km, fun_fact)
+
+  solar_system.add_planet(new_planet)
+end
+
 def main
   solar_system = SolarSystem.new('Sol')
 
@@ -32,18 +51,20 @@ def main
   'What would you like to do next?
     1. list planets
     2. planet details
-    3. exit'
+    3. add planet
+    4. exit'
 
   puts prompt
   input = gets.chomp.downcase
 
-  while input != "exit"
-
+  until input == "exit" || input == "4"
     case input
     when "1", "list planets"
       puts solar_system.list_planets
     when "2", "planet details"
       planet_details(solar_system)
+    when "3", "add planet"
+      add_planet_option(solar_system)
     else
       puts "#{input} is not an option."
     end
@@ -54,45 +75,5 @@ def main
 
   puts "Thank you for using the solar system program!"
 end
-
-# # # ... do stuff with planets ...
-# # earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
-# #
-# # puts earth.name # Earth
-# # puts "Color of #{earth.name} is #{earth.color}."
-# # puts earth.summary
-#
-# solar_system = SolarSystem.new('Sol')
-#
-# earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
-# mars = Planet.new('Mars', 'red', 6.39e23, 1.416e8, 'Pieces of Mars have been found on Earth')
-# venus = Planet.new('Venus', 'gray-ish', 4.867e24, 6.724e7, 'Second brightest object in the night sky after the Moon')
-# jupiter = Planet.new('Jupiter', 'red-orange/pizza-like', 1.898e27, 4.838e8, 'It is the fastest spinning planet in the solar system')
-# # eArth = Planet.new('eArth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
-# # eARth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
-# solar_system.add_planet(earth)
-# solar_system.add_planet(mars)
-# solar_system.add_planet(venus)
-# solar_system.add_planet(jupiter)
-# # solar_system.add_planet(eArth)
-# # solar_system.add_planet(eARth)
-#
-# puts solar_system.planets.class
-#
-# list = solar_system.list_planets
-# puts list
-#
-# found_planet = solar_system.find_planet_by_name('Earth')
-#
-# # found_planet is an instance of class Planet
-# puts found_planet
-# # found_planet.name('Bob')
-# puts found_planet.summary
-#
-# distance = solar_system.distance_between('Venus', 'Jupiter')
-# puts "The distance between Venus and Jupiter is #{distance}."
-#
-# distance = solar_system.distance_between('Jupiter', 'Earth')
-# puts "The distance between Jupiter and Earth is #{distance}."
 
 main
