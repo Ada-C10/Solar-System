eb18a4ee9e00f86e592890337fa4c2b424b88790
Katricia Smith <smithkatricia@gmail.com>
2018-08-21 16:10:15 -0700

    Added all planet instances to main method

--- main.rb
+++ main.rb
@@ -3,22 +3,19 @@ require_relative 'planet'
 require_relative 'solar_system.rb'
 
 def main
-  mercury = Planet.new("Mercury", "grey", 3.3011e23, 5.79e7, "is named after a Roman God")
-
-  venus = Planet.new("Venus", "yellowish-white", 4.87e24, 1.082e8, "may have had oceans in the past")
-
-  puts "#{mercury.name} #{mercury.fun_fact}."
-
-  puts "#{venus.name} #{venus.fun_fact}."
-
-  puts mercury.summary
-
-  puts venus.summary
+  # mercury = Planet.new("Mercury", "grey", 3.3011e23, 5.79e7, "is named after a Roman God")
+  #
+  # venus = Planet.new("Venus", "yellowish-white", 4.87e24, 1.082e8, "may have had oceans in the past")
+  #
+  # puts "#{mercury.name} #{mercury.fun_fact}."
+  #
+  # puts "#{venus.name} #{venus.fun_fact}."
+  #
+  # puts mercury.summary
+  #
+  # puts venus.summary
 
   solar_system = SolarSystem.new('Sol')
-
-  earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
-  solar_system.add_planet(earth)
 
   mercury = Planet.new("Mercury", "grey", 3.3011e23, 5.79e7, "is named after a Roman God")
   solar_system.add_planet(mercury)
@@ -26,6 +23,23 @@ def main
   venus = Planet.new("Venus", "yellowish-white", 4.87e24, 1.082e8, "may have had oceans in the past")
   solar_system.add_planet(venus)
 
+  earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'is the only planet known to support life')
+  solar_system.add_planet(earth)
+
+  mars = Planet.new('Mars', 'pale-yellow', 6.42e23, 2.279e8, 'has a large amount of underground ice')
+  solar_system.add_planet(mars)
+
+  jupiter = Planet.new('Jupiter', 'orange-white', 1.8982e27, 7.786e8, 'has at least 79 moons')
+  solar_system.add_planet(jupiter)
+
+  saturn = Planet.new('Saturn', 'pale-gold', 5.6834e26, 1.433e9, 'has a ring system made of ice, rock, and dust')
+  solar_system.add_planet(saturn)
+
+  uranus = Planet.new('Uranus', 'pale-blue', 8.68e25, 2.873e9, 'is visible to the naked eye')
+  solar_system.add_planet(uranus)
+
+  neptune = Planet.new('Neptune', 'pale-blue', 1.03e26, 4.495e9, 'is the farthest planet from the Sun that we know of in our solar system')
+  solar_system.add_planet(neptune)
 
   list = solar_system.list_planets
   puts list
