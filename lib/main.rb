f5722060e6d8807f3443ed68c69a9701a2dd985c
Aurea <fu.sakura@gmail.com>
2018-08-21 14:02:41 -0700

    add 3 new planets to main

--- lib/main.rb
+++ lib/main.rb
@@ -3,15 +3,17 @@ require_relative 'solar_system'
 
 def main
 
-  solar_system = SolarSystem.new(5)
+  solar_system = SolarSystem.new('Sol')
 
   earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
+  mars = Planet.new('Mars', 'red', 6.4171e23, 2.279e8, 'May have previously held water')
+  uranus = Planet.new('Uranus', 'light-blue', 8.6810e25, 2.871e8, 'First planet discovered by telescope')
+
   solar_system.add_planet(earth)
+  solar_system.add_planet(mars)
+  solar_system.add_planet(uranus)
 
-  list = solar_system.list_planets
-  puts list
-
-  puts solar_system.find_planet_by_name('Earth').summary
+  
 end
 
 main
--- specs/planet_spec.rb
+++ specs/planet_spec.rb
@@ -27,7 +27,7 @@ describe 'Planet Class' do
       }.must_raise(ArgumentError)
 
       expect{
-        Planet.new('Earth', 'blue-green', 0, -1.496e8, 'Only planet known to support life')
+        Planet.new('Earth', 'blue-green', 'asd', -1.496e8, 'Only planet known to support life')
        }.must_raise(ArgumentError)
     end
 
@@ -36,6 +36,5 @@ describe 'Planet Class' do
       earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
       expect ( earth.summary ).must_be_kind_of(String)
     end
-
   end
 end
