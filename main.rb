2ef3262edf5a8929566b4c69fecd1980d1fb31cf
Katrina Aganon <katrina.aganon@gmail.com>
2018-08-21 14:56:57 -0700

    Updated driver code to create instance of SolarSystem + printed a list

--- main.rb
+++ main.rb
@@ -1,12 +1,16 @@
 require_relative 'planet'
+require_relative 'solar_system'
 
 def main
   mars = Planet.new("Mars","reddish-brown", 6.39e23, 2.279e8, "Named after a mythological figure - the Roman god of war"  )
   jupiter = Planet.new("Jupiter", "shades of white, brown, and yellow", 1.898e27, 7.785e8, "It does not have a true surface, as the planet is mostly swirling gases and liquids")
-  puts mars.summary
-  puts
-  puts jupiter.summary
-  puts
+
+  solar_system = SolarSystem.new("Sol")
+  solar_system.add_planet(mars)
+  solar_system.add_planet(jupiter)
+  list = solar_system.list_planets
+  puts list
+
 end
 
 main
