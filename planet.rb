a7c7ce009facb66d4679dcb00610932630a6f9c3
BarbaraWidjono <barbarawidjono@gmail.com>
2018-08-21 11:00:51 -0700

    Created Planet class with constructor method

---
+++ planet.rb
@@ -0,0 +1,12 @@
+# Info about a single planet
+class Planet
+  def initialize(name, color, mass_kg, distance_from_sun_km, fun_fact)
+
+    @name = name
+    @color = color
+    @mass_kg = mass_kg
+    @distance_from_sun_km = distance_from_sun_km
+    @fun_fact = fun_fact
+  end
+
+end
