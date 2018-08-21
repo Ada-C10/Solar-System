af14b1e9f7b5bc897d98d87e02a232836f055655
jackie <jacquelyn.cheng@me.com>
2018-08-21 14:39:45 -0700

    Completed Wave 3, Step 1

--- lib/main.rb
+++ lib/main.rb
@@ -2,32 +2,65 @@ require_relative 'planet'
 require_relative 'solar_system'
 
 def main
-  # Create two Planets and a SolarSystem
-  earth = Planet.new("Earth", "blue-green", 5.972*10**24,
-    1.496*10**8, "Only planet known to support life")
+  solar_system = SolarSystem.new("The Sun")
+  
+  mercury = Planet.new("Mercury", "dark grey", 3.285*10**23, 57910000,
+    "Closest planet to The Sun")
+  venus = Planet.new("Venus", "pale yellow", 4.867*10**24, 108200000,
+    "Named after the goddess of love")
+  earth = Planet.new("Earth", "blue-green", 5.972*10**24, 1.496*10**8,
+    "Only planet known to support life")
+  mars = Planet.new("Mars", "red", 6.39*10**23, 227900000,
+    "Believed to have once had water")
   jupiter = Planet.new("Jupiter", "orange-ish", 1.898*10**27, 778500000,
-    "Largest planet")
-  earth2 = Planet.new("earth", "green-blue", 5.972*10**24,
-    1.496*10**8, "twin")
-  solar_system = SolarSystem.new("Sun")
+    "Largest planet in the solar system")
 
-  # Add earth and jupiter to solar_system
+  solar_system.add_planet(mercury)
+  solar_system.add_planet(venus)
   solar_system.add_planet(earth)
+  solar_system.add_planet(mars)
   solar_system.add_planet(jupiter)
-  solar_system.add_planet(earth2)
 
-  # Print outputs
-  puts earth.summary
-  puts jupiter.summary
-  puts solar_system.list_planets
-
-  # Finds all Planets in solar_system with a given name and prints output
-  found_planet = solar_system.find_planet_by_name("earth")
-  found_planet.each { |planet| puts planet.summary }
-
-  # Calculates and prints the distance between Earth and Jupiter
-  distance = solar_system.distance_between("earth", "jupiter")
-  puts "#{distance}km"
+  input = ""
+  until input.casecmp? "exit"
+    print "What would you like to do next?
+    Type 'list' to list planets or 'exit' to end the program: "
+    input = gets.chomp
+    if input.casecmp? "list"
+      puts solar_system.list_planets
+    end
+  end
 end
+
+# Wave 1 and 2 version of main:
+#
+# def main
+#   # Create two Planets and a SolarSystem
+#   earth = Planet.new("Earth", "blue-green", 5.972*10**24,
+#     1.496*10**8, "Only planet known to support life")
+#   jupiter = Planet.new("Jupiter", "orange-ish", 1.898*10**27, 778500000,
+#     "Largest planet")
+#   earth2 = Planet.new("earth", "green-blue", 5.972*10**24,
+#     1.496*10**8, "twin")
+#   solar_system = SolarSystem.new("Sun")
+#
+#   # Add earth and jupiter to solar_system
+#   solar_system.add_planet(earth)
+#   solar_system.add_planet(jupiter)
+#   solar_system.add_planet(earth2)
+#
+#   # Print outputs
+#   puts earth.summary
+#   puts jupiter.summary
+#   puts solar_system.list_planets
+#
+#   # Finds all Planets in solar_system with a given name and prints output
+#   found_planet = solar_system.find_planet_by_name("earth")
+#   found_planet.each { |planet| puts planet.summary }
+#
+#   # Calculates and prints the distance between Earth and Jupiter
+#   distance = solar_system.distance_between("earth", "jupiter")
+#   puts "#{distance}km"
+# end
 
 main
