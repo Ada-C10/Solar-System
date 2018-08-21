5fd48edae2b2f96272abc0375909461010277629
BarbaraWidjono <barbarawidjono@gmail.com>
2018-08-21 11:40:36 -0700

    Created instance method add_planet

--- solar_system.rb
+++ solar_system.rb
@@ -1,7 +1,16 @@
 # keeping track of a collection of instances of Planet
 class SolarSystem
+
+  # Can read these instance variables but cannot write on them
+  attr_reader :star_name, :planets
+
   def initialize(star_name)
     @star_name = star_name
+    @planets = []
+  end
+
+  def add_planet(instance_of_Planet)
+    @planets << instance_of_Planet
   end
 
 end
