e888417f68e8caffc385c0c1801a58fd868f8601
Katricia Smith <smithkatricia@gmail.com>
2018-08-21 12:33:29 -0700

    Added SolarSystem#list_planets

--- solar_system.rb
+++ solar_system.rb
@@ -8,8 +8,19 @@ class SolarSystem
     @planets = []
   end
 
+  # Create a method SolarSystem#add_planet, which will take an instance of Planet as a parameter and add it to the list of planets.
   def add_planet(Planet)
-    planets << Planet 
+    planets << Planet
+  end
+
+  # Create a method SolarSystem#list_planets, which will return (not puts) a string containing a list of all the planets in the system.
+  def list_planets
+    # Planets orbiting <star name>
+    # 1.  Mercury
+    # 2.  Venus
+    # 3.  Earth
+    # 4.  Mars
+    # 5.  Jupiter
   end
 
 end
