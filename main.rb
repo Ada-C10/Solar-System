31feed3b9dc407a446535cc08b986b03ba591e7d
Karis Kim <ouniiq@hotmail.com>
2018-08-21 14:08:06 -0700

    create two different instances of planet.rb and print out some of their attributes

--- main.rb
+++ main.rb
@@ -1,5 +1,13 @@
 require_relative 'planet'
 
 def main
-
+  earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'It\'s the only planet known to support life')
+  puts earth.name
+  puts earth.color
+  
+  venus = Planet.new('Venus', 'yellowish white', 4.867e24, 6.724e7, 'It\'s the second brightest object in the night sky after the Moon')
+  puts venus.name
+  puts venus.color
 end
+
+main
