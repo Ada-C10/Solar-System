7946793269343c588d4e900b9e0db0a504795e84
BarbaraWidjono <barbarawidjono@gmail.com>
2018-08-21 11:22:24 -0700

    Created 'main' method that instantiates the Planet class

---
+++ main.rb
@@ -0,0 +1,12 @@
+require_relative 'planet.rb'
+
+def main(name, color, mass_kg, distance_from_sun_km, fun_fact)
+  instance = Planet.new(name, color, mass_kg, distance_from_sun_km, fun_fact)
+  return instance
+end
+
+planet_1 = main("Mercury", "Gray", 0.330, 57.9, "Smallest planet in our Solar System")
+puts "#{planet_1}"
+puts "#{planet_1.name}"
+
+planet_2 = main("Venus", "Light Gray", 4.87, 108.2, "Spins in an opposite direction as compared to all other planets in the Solar System")
