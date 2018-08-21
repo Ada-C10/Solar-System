51036cea21297c01792c4e162134ea1d553098eb
Karis Kim <ouniiq@hotmail.com>
2018-08-21 14:50:04 -0700

    add_planet method created, stores planet info in planets array

--- solar_system.rb
+++ solar_system.rb
@@ -3,6 +3,10 @@ require_relative 'planet'
 class SolarSystem
   attr_reader :planets, :star_name
 
+  def add_planet(planet)
+    @planets << planet
+  end
+
   def initialize(star_name)
     @star_name = star_name
     @planets = []
