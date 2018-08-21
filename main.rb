3c32972e38e11706ae2b28214963aa12d6f1410c
Alice Hsiao <alice.hsiao87@gmail.com>
2018-08-21 14:31:50 -0700

    completed wave 2 and the optional task distance_between

--- main.rb
+++ main.rb
@@ -1,23 +1,37 @@
 require_relative 'planet.rb'
 require_relative 'solar_system.rb'
+require 'pry'
 
 def main
   solar_system = SolarSystem.new('Sol')
 
   mars = Planet.new('Mars', 'red', 6.42e23, 2.279e8, 'Possible subsurface lake!')
   jupiter = Planet.new('Jupiter', 'white-red-orange-brown-yellow', 1.898e28, 7.78e8, 'it is massive - 2.5 more massive than all of the other planets in the Solary System combined')
-  puts "Mass of Mars: #{mars.mass_kg}"
+  earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
+  earth2 = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
+
+  # puts "Mass of Mars: #{mars.mass_kg}"
   # puts "Fun Fact about Jupiter: #{jupiter.fun_fact}"
 
   # puts mars.summary # Why do we puts in main but not in planet summary
   # puts earth.summary
 
   solar_system.add_planet(mars)
-  # solar_system.add_planet(jupiter)
-  # solar_system.add_planet(earth)
+  solar_system.add_planet(jupiter)
+  solar_system.add_planet(earth)
+  solar_system.add_planet(earth2)
 
   list = solar_system.list_planets
-  # list
+  puts list
+
+  found_planet = solar_system.find_planet_by_name('water')
+  puts found_planet
+  if found_planet.class != String
+    puts found_planet.length > 1 ? found_planet[0].summary : found_planet.summary
+  end
+
+  final_distance = solar_system.distance_between('earth', 'jupiter')
+  puts "Distance between these two planets: #{'%.2f' % final_distance} km"
 
 end
 
--- solar_system.rb
+++ solar_system.rb
@@ -11,7 +11,37 @@ class SolarSystem
   end
 
   def list_planets
-    return "Planets orbiting #{@star_name} \n #{@planets.each {|planet| planet.color}}"
+    formatted_string = "Planets orbiting #{@star_name} \n"
+    @planets.each_with_index do |planet, i|
+      formatted_string += "#{i+1}. #{planet.name} \n"
+    end
+    return formatted_string
+  end
+
+  def find_planet_by_name (planet_name)
+    all_matched_planets = @planets.select do |individual_planet|
+      individual_planet.name == planet_name.capitalize
+    end
+    if all_matched_planets == []
+      return "No matches found"
+    elsif all_matched_planets.length == 1
+      return all_matched_planets[0]
+    else
+      return all_matched_planets
+    end
+  end
+
+  def distance_between (planet_one, planet_two)
+    first_planet_info = @planets.select do |individual_planet|
+      individual_planet.name == planet_one.capitalize
+    end
+    distance_a = first_planet_info[0].distance_from_sun_km
+    second_planet_info = @planets.select do |individual_planet|
+      individual_planet.name == planet_two.capitalize
+    end
+    distance_b = second_planet_info[0].distance_from_sun_km
+    return (distance_a - distance_b).abs
+
   end
 
 end
