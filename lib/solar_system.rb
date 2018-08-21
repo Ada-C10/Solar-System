46ee9c1aa12614831269c00b62c617c3e96979a0
Lindsay Terchin <lterchin@gmail.com>
2018-08-21 15:45:37 -0700

    created method to look up a planet

--- lib/solar_system.rb
+++ lib/solar_system.rb
@@ -1,24 +1,32 @@
 class SolarSystem
 
-  def initialize (star_name)
+  def initialize(star_name)
     @star_name = star_name
     @planets = []
   end
 
-
-  def add_planet (planet)
+  def add_planet(planet)
     @planets << planet
   end
 
   def list_planets
-    list = []
+    @list = []
     @planets.each_with_index do |planet, index|
       x = "#{(index + 1)}. #{planet.name}"
-      list << x
+      @list << x
 
     end
     return "Planets orbiting #{@star_name}:
-    #{list}"
+    #{@list}"
+  end
+
+  def find_planet_by_name(name)
+    @planets.each do |planet|
+      if planet.name.upcase == name.upcase
+        @found_planet = planet
+      end
+    end
+    return @found_planet
   end
 
   attr_reader :star_name, :planets
