cd2ff8caba330f1098f76f8f75129f0ebc9fa1c2
Katricia Smith <smithkatricia@gmail.com>
2018-08-21 12:27:39 -0700

    Added reader helper and #add_planet to class SolarSystem

--- solar_system.rb
+++ solar_system.rb
@@ -1,9 +1,15 @@
 class SolarSystem
 
+  attr_reader :star_name, :planets
+
   #The constructor should take one parameter, star_name, and save it in an instance variable.
   def initialize(star_name)
     @star_name = star_name
+    @planets = []
   end
 
+  def add_planet(Planet)
+    planets << Planet 
+  end
 
 end
