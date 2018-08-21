e0b0626ebef2cdfd2e4b92465f9b45042b1e432b
Divya <divyamohanan@gmail.com>
2018-08-21 16:14:17 -0700

    Requirements met + 1 optional

--- planet.rb
+++ planet.rb
@@ -1,15 +1,12 @@
 class Planet
   #constructor
-  def initialize(name, color, mass_kg,distance_from_sun_km, fun_fact)
+  def initialize(name, color, mass_kg, distance_from_sun_km, fun_fact)
 
-    while mass_kg <= 0
-      puts "Mass must be greater than 0. Enter mass again: "
-      mass_kg = gets.chomp.to_f
+    if mass_kg <= 0
+      raise ArgumentError "Mass should be bigger than 0"
     end
-    while distance_from_sun_km <= 0
-      puts "Distance must be greater than 0. Enter distance again: "
-      distance_from_sun_km = gets.chomp.to_f
-      return
+    if distance_from_sun_km <= 0
+      raise ArgumentError "Distance should be bigger than 0"
     end
     @name = name
     @color = color
@@ -18,23 +15,23 @@ class Planet
     @fun_fact = fun_fact
   end
 
-  attr_reader :name, :color, :mass_kg, :distance_from_sun_km, :fun_fact
+  #attr_reader :name, :color, :mass_kg, :distance_from_sun_km, :fun_fact
 
-  # def name
-  #   return @name
-  # end
-  # def color
-  #   return @color
-  # end
-  # def mass_kg
-  #   return @mass_kg
-  # end
-  # def distance_from_sun_km
-  #   return @distance_from_sun_km
-  # end
-  # def fun_fact
-  #   return @fun_fact
-  # end
+  def name
+    return @name
+  end
+  def color
+    return @color
+  end
+  def mass_kg
+    return @mass_kg
+  end
+  def distance_from_sun_km
+    return @distance_from_sun_km
+  end
+  def fun_fact
+    return @fun_fact
+  end
   def summary
     return "#{name} is planet that is #{color} ,weighs #{mass_kg} and is #{distance_from_sun_km}
     away from the sun, it is #{fun_fact}"
