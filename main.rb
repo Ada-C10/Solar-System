e4f8eaa4470872b7cd94e4469e33a68378f5e25c
Alice Hsiao <alice.hsiao87@gmail.com>
2018-08-21 15:14:57 -0700

    for wave 3, restructured main to enter a control loop that asks the user to either list the planets or exit

--- main.rb
+++ main.rb
@@ -8,28 +8,57 @@ def main
   mars = Planet.new('Mars', 'red', 6.42e23, 2.279e8, 'Possible subsurface lake!')
   jupiter = Planet.new('Jupiter', 'white-red-orange-brown-yellow', 1.898e28, 7.78e8, 'it is massive - 2.5 more massive than all of the other planets in the Solary System combined')
   earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
-  earth2 = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
-
-  # puts "Mass of Mars: #{mars.mass_kg}"
-  # puts "Fun Fact about Jupiter: #{jupiter.fun_fact}"
-
-  # puts mars.summary # Why do we puts in main but not in planet summary
-  # puts earth.summary
 
   solar_system.add_planet(mars)
   solar_system.add_planet(jupiter)
   solar_system.add_planet(earth)
-  solar_system.add_planet(earth2)
 
-  list = solar_system.list_planets
-  puts list
+  command = ''
+  while command != 'e' do
+    puts "What would you like to do next?"
+    print "Type 'a' if you would like to list the planets, or 'e' to exit: "
+    command = gets.chomp.downcase
+    if command == 'e'
+      break
+    elsif command == 'a'
+      list = solar_system.list_planets
+      puts list
+    else
+      puts "That is not a valid choice."
+    end
+  end
 
-  found_planet = solar_system.find_planet_by_name('earth')
-  puts found_planet
-  puts found_planet[0].summary
 
-  final_distance = solar_system.distance_between('earth', 'jupiter')
-  puts "Distance between Earth and Jupiter these two planets: #{'%.2f' % final_distance} km"
+
+
+
+  # solar_system = SolarSystem.new('Sol')
+  #
+  # mars = Planet.new('Mars', 'red', 6.42e23, 2.279e8, 'Possible subsurface lake!')
+  # jupiter = Planet.new('Jupiter', 'white-red-orange-brown-yellow', 1.898e28, 7.78e8, 'it is massive - 2.5 more massive than all of the other planets in the Solary System combined')
+  # earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
+  # earth2 = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
+  #
+  # # puts "Mass of Mars: #{mars.mass_kg}"
+  # # puts "Fun Fact about Jupiter: #{jupiter.fun_fact}"
+  #
+  # # puts mars.summary # Why do we puts in main but not in planet summary
+  # # puts earth.summary
+  #
+  # solar_system.add_planet(mars)
+  # solar_system.add_planet(jupiter)
+  # solar_system.add_planet(earth)
+  # solar_system.add_planet(earth2)
+  #
+  # list = solar_system.list_planets
+  # puts list
+  #
+  # found_planet = solar_system.find_planet_by_name('earth')
+  # puts found_planet
+  # puts found_planet[0].summary
+  #
+  # final_distance = solar_system.distance_between('earth', 'jupiter')
+  # puts "Distance between Earth and Jupiter these two planets: #{'%.2f' % final_distance} km"
 
 end
 
