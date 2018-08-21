61611bd2da62e807300bbd07516a54d0a7ae9b63
Katricia Smith <smithkatricia@gmail.com>
2018-08-21 12:19:03 -0700

    Added constructor with parameter star_name to class SolarSystem

--- solar_system.rb
+++ solar_system.rb
@@ -1,4 +1,9 @@
 class SolarSystem
 
+  #The constructor should take one parameter, star_name, and save it in an instance variable.
+  def initialize(star_name)
+    @star_name = star_name
+  end
+
 
 end
