c07c4533e550283b918c73188ef9831777b8d864
BarbaraWidjono <barbarawidjono@gmail.com>
2018-08-21 11:29:43 -0700

    Running driver code to test planet.rb

--- main.rb
+++ main.rb
@@ -8,5 +8,12 @@ end
 planet_1 = main("Mercury", "Gray", 0.330, 57.9, "Smallest planet in our Solar System")
 puts "#{planet_1}"
 puts "#{planet_1.name}"
+puts "#{planet_1.summary}"
+puts
+
 
 planet_2 = main("Venus", "Light Gray", 4.87, 108.2, "Spins in an opposite direction as compared to all other planets in the Solar System")
+puts "#{planet_2}"
+puts "#{planet_2.name}"
+puts "#{planet_2.summary}"
+puts
