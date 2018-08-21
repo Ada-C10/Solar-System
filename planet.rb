ae63a3b51c9852b8da9dd11326261c5f4ea41174
Karis Kim <ouniiq@hotmail.com>
2018-08-21 14:42:44 -0700

    created solar_system.rb

--- planet.rb
+++ planet.rb
@@ -1,4 +1,5 @@
 require 'pry'
+
 class Planet
 
   attr_reader :name, :color, :mass_kg, :distance_from_sun_km, :fun_fact
@@ -10,9 +11,9 @@ class Planet
     @distance_from_sun_km = distance_from_sun_km
     @fun_fact = fun_fact
   end
-end
 
   def summary
    return "#{@name} is a #{@color} planet that weighs #{@mass_kg}. It's #{@distance_from_sun_km} km away from the sun. Fun fact about #{@name}: #{@fun_fact}"
+  end
  end
 binding.pry
