b66412c5b5e823505b1b0fb1b76731a9a3ead9a7
xLJohnsonx <lizjohnsonemail@gmail.com>
2018-08-21 14:24:38 -0700

    check valid test complete for mass and distance

--- planet.rb
+++ planet.rb
@@ -1,3 +1,5 @@
+# In a file called planet.rb, create a class called Planet. Each instance of this class will keep track of
+# information about a single planet
 class Planet
   attr_reader :name, :color, :mass_kg, :distance_from_sun_km, :fun_fact
   def initialize(name, color, mass_kg, distance_from_sun_km, fun_fact)
@@ -6,12 +8,15 @@ class Planet
     @mass_kg = mass_kg
     @distance_from_sun_km = distance_from_sun_km
     @fun_fact = fun_fact
+    
   end
+
   def check_valid(mass_kg, distance_from_sun_km)
     if mass_kg <= 0 || distance_from_sun_km <= 0
       raise ArgumentError, 'values must be > 0'
     end
   end
+
   def summary
     "The planet #{@name} is the color #{@color} and weighs #{@mass_kg} kilograms.\
  #{@name} is #{@distance_from_sun_km} kilometers from the sun and interestingly, #{@fun_fact}."
--- planet_spec.rb
+++ planet_spec.rb
@@ -5,18 +5,19 @@ require_relative 'planet.rb'
 Minitest::Reporters.use!
 
 describe 'planet tests' do
-  it 'must have positive values' do
+  it 'must have positive values greater than 0' do
     # ARRANGE
-    @name = 'Mercury'
-    @color = 'dark grey'
-    @mass_kg = -4.345
-    @distance_from_sun_km = -23423
-    @fun_fact = 'has an earth year of about 88 days'
-
+    name = 'Mercury'
+    color = 'dark grey'
+    mass_kg = -4.345
+    distance_from_sun_km = -23423
+    fun_fact = 'has an earth year of about 88 days'
+    # instantiate mercury
+    mercury = Planet.new(name, color, mass_kg,
+               distance_from_sun_km, fun_fact)
     # ACT & ASSERT
     expect do
-      Planet.new(@name, @color, @mass_kg,
-                 @distance_from_sun_km, @fun_fact)
+      mercury.check_valid(mass_kg, distance_from_sun_km)
     end.must_raise ArgumentError
   end
 end
