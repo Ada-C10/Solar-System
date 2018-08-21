cc4965f02d6501f003f9a253bd7f05f5ecd300fc
Karis Kim <ouniiq@hotmail.com>
2018-08-21 11:47:39 -0700

    tested with pry and all instance variables are readable only

--- planet.rb
+++ planet.rb
@@ -1,4 +1,8 @@
+require 'pry'
 class Planet
+
+  attr_reader :name, :color, :mass_kg, :distance_from_sun_km, :fun_fact
+
   def initialize(name, color, mass_kg, distance_from_sun_km, fun_fact)
     @name = name
     @color = color
@@ -7,3 +11,5 @@ class Planet
     @fun_fact = fun_fact
   end
 end
+
+binding.pry
