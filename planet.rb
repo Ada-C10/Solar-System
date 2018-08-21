f03b53604eebc5daaf5538b325b243038f3b9385
BarbaraWidjono <barbarawidjono@gmail.com>
2018-08-21 15:53:18 -0700

    Included the .capitalize method onto the constructor for @name

--- planet.rb
+++ planet.rb
@@ -6,7 +6,7 @@ class Planet
 
   # Constructor method
   def initialize(name, color, mass_kg, distance_from_sun_km, fun_fact)
-    @name = name
+    @name = name.capitalize
     @color = color
     @mass_kg = mass_kg
     @distance_from_sun_km = distance_from_sun_km
