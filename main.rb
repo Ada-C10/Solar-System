40d110f7e2b8cb2a94e39acbdc079e62f0d6d148
Katricia Smith <smithkatricia@gmail.com>
2018-08-21 12:04:37 -0700

    Added two instances of planet to main method in main.rb - Venus and Mercury

--- main.rb
+++ main.rb
@@ -2,7 +2,13 @@
 require_relative 'planet'
 
 def main
-  # ... do stuff with planets ...
+  mercury = Planet.new("Mercury", "grey", 3.3011e23, 5.79e7, "is named after a Roman God")
+
+  venus = Planet.new("Venus", "yellowish-white", 4.87e24, 1.082e8, "may have had oceans in the past")
+
+  puts "#{mercury.name} #{mercury.fun_fact}."
+
+  puts "#{venus.name} #{venus.fun_fact}."
 end
 
 main
