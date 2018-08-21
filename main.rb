ab8dbe556bf247a9df9afc58f463c941d836aec0
BarbaraWidjono <barbarawidjono@gmail.com>
2018-08-21 14:50:26 -0700

    Changing implementation of 'main' method to include command line interface: inserting greeting for user

--- main.rb
+++ main.rb
@@ -2,31 +2,25 @@ require_relative 'planet.rb'
 require_relative 'solar_system.rb'
 
 def main
-
-  # First instanciation of Planet class
-  planet_1 = Planet.new("Mercury", "Gray", 0.330, 57.9, "Smallest planet in our Solar System")
-  puts "#{planet_1}"
-  puts "#{planet_1.name}"
-  puts "#{planet_1.summary}"
-  puts
-
-  # Second instanciation of Planet class
-  planet_2 = Planet.new("Venus", "Light Gray", 4.87, 108.2, "Spins in an opposite direction as compared to all other planets in the Solar System")
-  puts "#{planet_2}"
-  puts "#{planet_2.name}"
-  puts "#{planet_2.summary}"
-  puts
-
-  # Instanciation of SolarSystem class
+  # Creating an instance of SolarSystem
   solar_system = SolarSystem.new("Sol")
 
-  # Adding a Planet object to the instance method 'add_planet' to the instance of SolarSystem which was assigned to the variable solar_system
-  solar_system.add_planet(planet_1)
-  solar_system.add_planet(planet_2)
+  # Creating first instance of Planet
+  planet_1 = Planet.new("Mercury", "Gray", 0.330, 57.9, "Smallest planet in our Solar System")
 
-  # Invoking the instance method 'list_planets' on the instance of SolarSystem which was assigned to the variable solar_system
-  list_of_planets = solar_system.list_planets
-  puts list_of_planets
+  # Creating second instance of Planet
+  planet_2 = Planet.new("Venus", "Light Gray", 4.87, 108.2, "Spins in an opposite direction as compared to all other planets in the Solar System")
+
+  # Welcome message
+  puts "******** Welcome to the Solar System Program! ********"
+  puts "These are the options:"
+  puts "1. View planet details"
+  puts "2. View summary for all planets"
+  puts "3. Add a planet"
+  puts "4. Exit program"
+  puts "Please make a selection: "
+  selection = gets.chomp
+
 end
 
 main
