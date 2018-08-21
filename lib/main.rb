73231705c0a6d8468578b8a56bb5dbd0c909226f
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-08-21 15:59:53 -0700

    Began draft of planet details option

--- lib/main.rb
+++ lib/main.rb
@@ -11,28 +11,40 @@ require_relative 'solar_system'
 def main
   ### Restructure ###
   # Creating solar system
-  solar_system = SolarSystem.new('Mario_World')
+  mario_system = SolarSystem.new('Mario_World')
 
   # Adding planets to solar system
-  solar_system.add_planet(Planet.new('Bowser', 'black', 9459e23, 345.9e8, 'A planet ruled by Boswer'))
-  solar_system.add_planet(Planet.new('Yoshi', 'green', 9.39e23, 227.9e8, 'A calm planet full habitated by Yoshis'))
+  mario_system.add_planet(Planet.new('Bowser', 'black', 9459e23, 345.9e8, 'A planet ruled by Boswer'))
+  mario_system.add_planet(Planet.new('Yoshi', 'green', 9.39e23, 227.9e8, 'A calm planet full habitated by Yoshis'))
 
 
-  def user_actions
+  def user_actions(solar_system)
     puts "What would you like to do?"
-    options = ["list planets", "exit"]
+    options = ["list planets", "planet details", "exit"]
     puts options.join(", ")
-    user_selection = gets.chomp.downcase
+    user_selection = gets.chomp.capitalize
 
     # Checking that user selection is valid
     while options.include?(user_selection) == false
-      puts "Please select a valid option #{options.join(", ")}"
+      puts "Please enter a valid option: #{options.join(", ")}"
       user_selection = gets.chomp.downcase
     end
 
+    case user_selection
+      # CALLING list planets method if user selection matches
+      when "list planets" then solar_system.list_planets
+      # Ask user for name of planet
+      when 'planet details'
+        puts "What planet would you like to learn about?"
+        planet_to_find = gets.chomp.downcase
+        # Display details for that planet
+        solar_system.find_planet_by_name(planet_to_find).summary
+    end
+  
+
   end
 
-  user_actions
+  puts user_actions(mario_system)
   # # Creating a list of planets
   # list = solar_system.list_planets
 
