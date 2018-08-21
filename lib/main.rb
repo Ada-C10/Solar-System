1082bab7e953826900396a42f4b28a4ae3e496ad
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-08-21 15:44:55 -0700

    Began reconstruction for wave 3

--- lib/main.rb
+++ lib/main.rb
@@ -1,33 +1,41 @@
 require_relative 'planet'
 require_relative 'solar_system'
 
+# Restructure main to do the following:
+#
+# Create a SolarSystem and add some Planets
+# Enter a control loop that repeatedly asks
+# the user what to do next.
+# The two options at this point are list planets and exit.
+
 def main
-  # Create an instance of planet
-  mars = Planet.new('Mars', 'red', 6.39e23, 227.9e8, 'Named after Roman god of war, Mars')
-  # Create another instance of planet
-  venus = Planet.new('Venus', 'white', 5.972e24, 1.08e8, 'Is the second brightest natural object in the sky')
+  ### Restructure ###
+  # Creating solar system
+  solar_system = SolarSystem.new('Mario_World')
 
-  # Create an instance of SolarSystem
-  solar_system = SolarSystem.new('Sol')
+  # Adding planets to solar system
+  solar_system.add_planet(Planet.new('Bowser', 'black', 9459e23, 345.9e8, 'A planet ruled by Boswer'))
+  solar_system.add_planet(Planet.new('Yoshi', 'green', 9.39e23, 227.9e8, 'A calm planet full habitated by Yoshis'))
 
 
-  # ADDING planets to solar system
-  solar_system.add_planet(venus)
-  solar_system.add_planet(mars)
+  def user_actions
+    puts "What would you like to do?"
+    options = ["list planets", "exit"]
+    puts options.join(", ")
+    user_selection = gets.chomp.downcase
 
-  # Creating a list of planets
-  list = solar_system.list_planets
-  # Printing planet list
-  puts list
+    # Checking that user selection is valid
+    while options.include?(user_selection) == false
+      puts "Please select a valid option #{options.join(", ")}"
+      user_selection = gets.chomp.downcase
+    end
 
-  # Printing planet summaries
-  puts mars.summary
-  puts venus.summary
+  end
 
-  # Checking if planet is in solar system
-  found_planet = solar_system.find_planet_by_name('mars')
-  puts found_planet
-  puts found_planet.summary
+  user_actions
+  # # Creating a list of planets
+  # list = solar_system.list_planets
+
 
 end
 
