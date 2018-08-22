a3d8e2bffe8b24d14aaac93884decddf8f6c3b0b
Madaleine Shields <maddiev.shields@gmail.com>
2018-08-22 13:06:11 -0700

    Add arguments for invalid numbers with helper methods

--- lib/planet.rb
+++ lib/planet.rb
@@ -4,6 +4,8 @@ class Planet
 
   def initialize(name, color, mass_kg, distance_from_sun_km, fun_fact)
 
+    invalid_argument if invalid_mass(mass_kg) || invalid_distance(distance_from_sun_km)
+
     @name = name
     @color = color
     @mass_kg = mass_kg
@@ -13,5 +15,17 @@ class Planet
 
   def summary
     return "\n#{@name} is a #{@color} planet.\nIt weighs #{@mass_kg} and is #{@distance_from_sun_km} km away from the sun!\nALSO, here's a really fun fact about #{@name}!\n#{fun_fact}"
+  end
+
+  def invalid_mass(mass_kg)
+    mass_kg <= 0
+  end
+
+  def invalid_distance(distance_from_sun_km)
+    distance_from_sun_km <= 0
+  end
+
+  def invalid_argument
+    raise ArgumentError, 'Argument should be greater than 0'
   end
 end
