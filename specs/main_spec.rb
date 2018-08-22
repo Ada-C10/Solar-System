46ae511e20ffdfbe59255076ec668dae2473766e
Madaleine Shields <maddiev.shields@gmail.com>
2018-08-22 11:29:23 -0700

    Write tests for main method to pass

--- specs/main_spec.rb
+++ specs/main_spec.rb
@@ -13,6 +13,16 @@ describe 'Main' do
   describe 'main method' do
     it 'displays a list of the planets created' do
 
+      solar_system = SolarSystem.new('Sol')
+
+      earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
+
+      solar_system.add_planet(earth)
+
+      list = solar_system.list_planets
+
+      list.must_be_kind_of Array
+      list.first.must_equal "1: Earth"
     end
   end
 end
