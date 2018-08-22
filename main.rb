97b05f7cdaff1db37129ccfe7d12b9fe146929d3
Katie Jahanmir <katietj@gmail.com>
2018-08-22 13:07:22 -0700

    Deleted old driver code

--- main.rb
+++ main.rb
@@ -2,20 +2,7 @@ require_relative 'planet'
 require_relative 'solar_system'
 
 def main
-  # pluto = Planet.new("Pluto", "Green", 112.2, 34343.6,"maybe pluto isn't even really a planet!")
-  # puts pluto.summary
-  #
-  # solar_system = SolarSystem.new('Sol')
-  # earth = Planet.new("Earth", "Blue", 343535.1, 35655.1, "Earth is on fire!")
-  # solar_system.add_planet(earth)
-  # solar_system.add_planet(pluto)
-  #
-  # list = solar_system.list_planets
-  # puts list
-  #
-  # found_planet = solar_system.find_planet_by_name('Earth')
-  # puts found_planet
-  # puts found_planet.first.summary
+  
   solar_system = SolarSystem.new('Milky Way')
   earth = Planet.new("Earth", "Blue", 12324.2, 23242.4, "Earth has lots of water!")
   jupiter = Planet.new("Jupiter", "Red", 1234535.4, 23.2, "Jupiter is so hot" )
