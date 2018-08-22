b1f8444a415e4aeb1b9d363aa59d8fc4535d068f
Addison Cole <addison.cole85@gmail.com>
2018-08-22 12:14:18 -0700

    add to main method, create a CLI that can add a planet and pull planet info, as well as list all planets in system.

--- main.rb
+++ main.rb
@@ -3,22 +3,68 @@ require_relative 'solar_system'
 require 'pry'
 
 def main
-star = SolarSystem.new('Sol')
 
-earth = Planet.new('Tattooine', 'sand dune', 5.972e24, 1.496e8, 'Has two suns and Han shot first.')
-mercury = Planet.new('Hoth', 'artic ice', 5.972e24, 1.496e8, 'You can ride wampas here')
-venus = Planet.new('Endor', 'forrest green', 5.972e24, 1.496e8, 'Home to Ewoks')
-mars = Planet.new('Kamino', 'stormy ocean', 5.972e24, 1.496e8, 'Home to industrial cloning')
+  star = SolarSystem.new('Sol')
 
-star.add_planet(earth)
-star.add_planet(mercury)
-star.add_planet(venus)
-star.add_planet(mars)
+  tatooine = Planet.new('Tatooine', 'sand dunes', 3.677e18, 1.496e8, 'Has two suns and Han shot first.')
+  hoth = Planet.new('Hoth', 'artic ice', 5.972e28, 4.447e10, 'You can ride wampas here')
+  endor = Planet.new('Endor', 'forrest green', 5.972e24, 1.496e11, 'Home to Ewoks')
+  kamino = Planet.new('Kamino', 'stormy ocean', 7.187e24, 9.96e8, 'Home to industrial cloning')
 
-list = star.list_planets
+  star.add_planet(tatooine)
+  star.add_planet(hoth)
+  star.add_planet(endor)
+  star.add_planet(kamino)
 
-found_planet = star.find_planet_by_name('hoth')
-puts found_planet.summary
+  exit_switch = false
+
+  puts "_** Universal Fictional Planetary Database **_\n"
+  puts "1. List Planets\n"
+  puts "2. Planet Information\n"
+  puts "3. Enter New Planet\n"
+  puts "4. Exit\n"
+  puts "************************************************"
+
+  while exit_switch == false do
+    print "Enter database selection: "
+    input = gets.chomp.to_i
+    if input == 1
+      puts star.list_planets
+    elsif input == 2
+      planet = get_planet_name(star)
+      puts planet.summary
+    elsif input == 3
+      add_a_planet(star)
+      puts "...Entered into system. Thank You."
+    elsif input == 4
+      exit_switch = true
+      exit
+    end
+  end
+end
+
+def get_planet_name(solar_system)
+  print "Enter planet name to access: "
+  planet = gets.chomp.to_s
+  return solar_system.find_planet_by_name(planet)
+end
+
+def add_a_planet(star)
+  print "Enter new planet name: "
+  planet_name = gets.chomp.to_s
+  print "Description of planet: "
+  color = gets.chomp.to_s
+  print "Distance of planet from it's star: "
+  distance = gets.chomp.to_f
+  print "Mass of planet: "
+  mass = gets.chomp.to_f
+  print "Enter any interesting features: "
+  fun_fact = gets.chomp.to_s
+
+  new_planet = Planet.new(planet_name, color, distance, mass, fun_fact)
+
+  star.add_planet(new_planet)
+  print "Data captured..."
 end
 
 main
