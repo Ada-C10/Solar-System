3bc8c81d94fe4897c94592d0b39646e1f371c2ef
Laura <larmartinez2@gmail.com>
2018-08-22 11:38:39 -0700

    refactored main.rb

--- main.rb
+++ main.rb
@@ -13,41 +13,14 @@ def main
   puts "option C: planet details"
   puts "option D: add planet"
   decision = gets.chomp
-  if decision.upcase == "C"
-    puts "which planet would you like to learn more about?"
-    planet = gets.chomp
-    found_planet = solar_system.find_planet_by_name(planet)
-    puts found_planet.summary
-  elsif decision.upcase == "D"
-    puts "So, you want to add a planet, I need to know more about it."
-    puts "Name?"
-    name = gets.chomp
-    puts "color?"
-    color = gets.chomp
-    puts "mass?"
-    mass = gets.chomp
-    puts "distance_from_sun_km?"
-    distance = gets.chomp
-    puts "fun_fact?"
-    fun_fact = gets.chomp
-    planet = Planet.new(name, color, mass, distance, fun_fact)
-    solar_system.add_planet(planet)
-  elsif decision.upcase == "A"
-    list = solar_system.list_planets
-    puts list
-  end
-  until decision.upcase == "B"
-    puts "What would you like to do? (A/B/C/D)"
-    puts "option A: list plannets"
-    puts "option B: exit"
-    puts "option C: planet details"
-    puts "option D: add planet"
-    decision = gets.chomp
+  while decision.upcase != "B"
     if decision.upcase == "C"
       puts "which planet would you like to learn more about?"
       planet = gets.chomp
       found_planet = solar_system.find_planet_by_name(planet)
       puts found_planet.summary
+      puts "What would you like to do? (A/B/C/D)\noption A: list plannets\noption B: exit\noption C: planet details\noption D: add planet"
+      decision = gets.chomp
     elsif decision.upcase == "D"
       puts "So, you want to add a planet, I need to know more about it."
       puts "Name?"
@@ -56,15 +29,19 @@ def main
       color = gets.chomp
       puts "mass?"
       mass = gets.chomp
-      puts "distance_from_sun_km?"
+      puts "distance from sun in km?"
       distance = gets.chomp
       puts "fun_fact?"
       fun_fact = gets.chomp
       planet = Planet.new(name, color, mass, distance, fun_fact)
       solar_system.add_planet(planet)
+      puts "What would you like to do? (A/B/C/D)\noption A: list plannets\noption B: exit\noption C: planet details\noption D: add planet"
+      decision = gets.chomp
     elsif decision.upcase == "A"
       list = solar_system.list_planets
       puts list
+      puts "What would you like to do? (A/B/C/D)\noption A: list plannets\noption B: exit\noption C: planet details\noption D: add planet"
+      decision = gets.chomp
     end
   end
 end
