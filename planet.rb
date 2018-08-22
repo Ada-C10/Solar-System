7c697f4c7ddd25c747bb57550e8f7f7204c8726e
Hannah <hannahvanseeters@gmail.com>
2018-08-22 09:08:22 -0700

    wave 3 complete

--- planet.rb
+++ planet.rb
@@ -1,3 +1,6 @@
+require 'terminal-table'
+require 'colorize'
+
 class Planet
   def initialize(name, color, mass_kg, distance_from_sun_km, fun_fact)
     @name = name
@@ -7,11 +10,23 @@ class Planet
     @fun_fact = fun_fact
   end
   def summary
-    return "    Name: #{@name}
+    return "\n    Name: #{@name}
     Color: #{@color}
     Mass: #{@mass}
     Distance: #{@distnace}
-    Fun_fact: #{@fun_fact}\n"
+    Fun_fact: #{@fun_fact}"
+    # rows = [@name, @color, @mass, @distnace, @fun_fact]
+    # table = Terminal::Table.new
+    # table.title = "Planet Details".colorize(:cyan)
+    # table.headings = ["Name".colorize(:green), "Color".colorize(:green),
+    #   "Mass".colorize(:green), "Distance".colorize(:green),
+    #   "Fun fact".colorize(:green)]
+    # table.rows = rows
+    # table.style = {:width => 60, :all_separators => true,
+    #   :border_x => "=".colorize(:red), :border_i =>
+    # "="}
+    #
+    # puts table
   end
 
   attr_reader :name, :color, :mass, :distance, :fun_fact
