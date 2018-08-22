ff9f094b43a5a32aef2acfe0ef623197f4158ade
Naheed Arang <arang.na@gmail.com>
2018-08-22 12:20:28 -0700

    add list_planets method

--- main.rb
+++ main.rb
@@ -1,6 +1,21 @@
 require_relative 'planet.rb'
+require_relative 'solar_system.rb'
+
 def main
   solar_system = SolarSystem.new('Sol')
 
-  earth = Planet.new('Earth', )
+  earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
+  mercury = Planet.new('Mercury', 'red', 4.454e90, 3.45e9, 'Mercury is a planet')
+  venus = Planet.new('Venus', 'orange', 6.43e93, 4.12e9, 'Venus is a planet')
+  mars = Planet.new('Mars', 'red', 4.238e82, 2.19e4, 'Mars is a planet')
+
+  solar_system.add_planet(earth)
+  solar_system.add_planet(mercury)
+  solar_system.add_planet(venus)
+  solar_system.add_planet(mars)
+
+  list = solar_system.list_planets
+  puts list
 end
+
+main
--- solar_system.rb
+++ solar_system.rb
@@ -1,13 +1,25 @@
-class SolarSystem(star_name)
-  def initialize
+class SolarSystem
+
+  attr_reader :star_name, :planets
+
+  def initialize(star_name)
     @star_name = star_name
     @planets = []
   end
 
-  def add_planet(Planet)
-    
+  def add_planet (Planet)
+    @planets << planet
+  end
 
   def list_planets
-    return "Planets orbiting #{star_name} /n "
+
+    list = "Planets orbiting #{@star_name}: \n "
+
+    @planets.each_with_index do |planet, index|
+      index += 1
+      list += "#{index}. #{planet.name} \n"
+    end
+    return list
+
   end
 end
