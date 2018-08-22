5fbb5b4765befb484794dfb2fc04e178b5f5ef30
Madaleine Shields <maddiev.shields@gmail.com>
2018-08-22 11:24:31 -0700

    Write passing tests for creating instance of Planet

--- specs/planet_spec.rb
+++ specs/planet_spec.rb
@@ -4,15 +4,15 @@ require 'minitest/skip_dsl'
 
 require_relative '../lib/planet'
 
-require 'pry'
-
-# Get that nice colorized output
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 describe 'Planet' do
   describe 'initialize method' do
     it 'takes 5 params to create an instance of a Planet' do
 
+      earth =  Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
+
+      earth.must_be_instance_of Planet
     end
   end
 end
