00a4d6e51c8fdc0c363170ac5d61b70c6a3b3b7b
Naheed Arang <arang.na@gmail.com>
2018-08-22 16:48:44 -0700

    add wave 2

--- main.rb
+++ main.rb
@@ -8,14 +8,20 @@ def main
   mercury = Planet.new('Mercury', 'red', 4.454e90, 3.45e9, 'Mercury is a planet')
   venus = Planet.new('Venus', 'orange', 6.43e93, 4.12e9, 'Venus is a planet')
   mars = Planet.new('Mars', 'red', 4.238e82, 2.19e4, 'Mars is a planet')
+  jupiter = Planet.new('Jupiter', 'brown', 6.492e30, 4.19e6, 'Jupiter is a planet')
 
   solar_system.add_planet(earth)
   solar_system.add_planet(mercury)
   solar_system.add_planet(venus)
   solar_system.add_planet(mars)
+  solar_system.add_planet(jupiter)
 
   list = solar_system.list_planets
   puts list
+
+  found_planet = solar_system.find_planet_by_name('mercury')
+  puts found_planet.summary
+
 end
 
 main
--- solar_system.rb
+++ solar_system.rb
@@ -7,19 +7,34 @@ class SolarSystem
     @planets = []
   end
 
-  def add_planet (Planet)
+  def add_planet(planet)
     @planets << planet
   end
 
   def list_planets
 
-    list = "Planets orbiting #{@star_name}: \n "
+    list = "Planets orbiting #{@star_name}:\n"
 
-    @planets.each_with_index do |planet, index|
-      index += 1
-      list += "#{index}. #{planet.name} \n"
+    @planets.each_with_index do |planet, number|
+      number += 1
+      list += "#{number}. #{planet.name}\n"
     end
     return list
 
+  end
+
+  def find_planet_by_name(planet)
+    planet.capitalize
+    @planets.each do |planet|
+      if @planets.include?(planet)
+      end
+      return planet
+    end
+
+    #input: string, #output: Planet
+    #goal: we match the input string to some planet in our array and return the planet if it exists
+    #loop through the planets array,
+    #check if the planet names match the input string,
+    #and if it matches, return the planet object
   end
 end
