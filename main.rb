0765a183986d09894724f6cac7d9e6de096f0bc1
Alice Hsiao <alice.hsiao87@gmail.com>
2018-08-21 19:57:22 -0700

    moved print to screen methods into main.rb and updated the condition to the while loop

--- main.rb
+++ main.rb
@@ -2,6 +2,38 @@ require_relative 'planet.rb'
 require_relative 'solar_system.rb'
 require 'pry'
 
+def print_distance (solar_system)
+  print "Enter first planet: "
+  planet_one = gets.chomp.capitalize
+  print "Enter second planet: "
+  planet_two = gets.chomp.capitalize
+  distance = solar_system.distance_between(planet_one, planet_two)
+  puts "The distance between #{planet_one} and #{planet_two} is #{'%.2f' % distance} km.\n\n"
+end
+
+def add_planet_to_list (solar_system)
+  print "What is the name of the planet? "
+  name = gets.chomp.capitalize
+  print "What color is the planet? "
+  color = gets.chomp
+  print "What is the mass in kg? "
+  mass = gets.chomp.to_f
+  print "What is the distance from the sun? "
+  distance_from_sun = gets.chomp.to_f
+  print "Name one fun fact about this planet: "
+  fun_fact = gets.chomp
+
+  new_planet = Planet.new(name, color, mass, distance_from_sun, fun_fact)
+  solar_system.add_planet(new_planet)
+end
+
+def print_planet_details(solar_system)
+  print "What is the name of the planet you wish to learn about? "
+  planet_name = gets.chomp
+  found_planet = solar_system.find_planet_by_name(planet_name)
+  puts "#{found_planet[0].summary}\n\n"
+end
+
 def main
   solar_system = SolarSystem.new('Sol')
 
@@ -13,8 +45,8 @@ def main
   solar_system.add_planet(jupiter)
   solar_system.add_planet(earth)
 
-  command = ''
-  while command != 5 do
+  keep_looping = true
+  while keep_looping do
     puts "What would you like to do next? Enter the number of your choice."
     puts "1. List the planets"
     puts "2. See planet details"
@@ -27,11 +59,11 @@ def main
     when 1
       puts "#{solar_system.list_planets}\n"
     when 2
-      solar_system.user_planet_details
+      print_planet_details(solar_system)
     when 3
-      solar_system.user_added_planet
+      add_planet_to_list(solar_system)
     when 4
-      solar_system.user_wanted_distance
+      print_distance(solar_system)
     when 5
       break
     else
--- solar_system.rb
+++ solar_system.rb
@@ -43,36 +43,4 @@ class SolarSystem
     return (distance_one - distance_two).abs
   end
 
-  def user_added_planet
-    print "What is the name of the planet? "
-    name = gets.chomp.capitalize
-    print "What color is the planet? "
-    color = gets.chomp
-    print "What is the mass in kg? "
-    mass = gets.chomp.to_f
-    print "What is the distance from the sun? "
-    distance_from_sun = gets.chomp.to_f
-    print "Name one fun fact about this planet: "
-    fun_fact = gets.chomp
-
-    new_planet = Planet.new(name, color, mass, distance_from_sun, fun_fact)
-    self.add_planet(new_planet)
-  end
-
-  def user_wanted_distance
-    print "Enter first planet: "
-    planet_one = gets.chomp.capitalize
-    print "Enter second planet: "
-    planet_two = gets.chomp.capitalize
-    distance = self.distance_between(planet_one, planet_two)
-    puts "The distance between #{planet_one} and #{planet_two} is #{'%.2f' % distance} km.\n\n"
-  end
-
-  def user_planet_details
-    print "What is the name of the planet you wish to learn about? "
-    planet_name = gets.chomp
-    found_planet = self.find_planet_by_name(planet_name)
-    puts "#{found_planet[0].summary}\n\n"
-  end
-
 end
