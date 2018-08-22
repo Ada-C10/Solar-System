0fef1ab5627463fc83cf8de9a86ea3586ef56a83
Naheed Arang <arang.na@gmail.com>
2018-08-22 11:26:08 -0700

    add wave 1

---
+++ main.rb
@@ -0,0 +1,6 @@
+require_relative 'planet.rb'
+def main
+  solar_system = SolarSystem.new('Sol')
+
+  earth = Planet.new('Earth', )
+end
---
+++ solar_system.rb
@@ -0,0 +1,13 @@
+class SolarSystem(star_name)
+  def initialize
+    @star_name = star_name
+    @planets = []
+  end
+
+  def add_planet(Planet)
+    
+
+  def list_planets
+    return "Planets orbiting #{star_name} /n "
+  end
+end
