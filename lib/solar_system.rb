c80db38632d12699050d92de0a1ee36a1318dc71
Madaleine Shields <maddiev.shields@gmail.com>
2018-08-22 09:14:42 -0700

    Change the list_planets to iterate over each with index

--- lib/solar_system.rb
+++ lib/solar_system.rb
@@ -12,8 +12,7 @@ class SolarSystem
 
   def list_planets
     planets_list = []
-    i = 0
-    @planets.each do |planet|
+    @planets.each_with_index do |planet, i|
       planets_list  << "#{i+1}: #{planet.name}"
     end
     return planets_list
