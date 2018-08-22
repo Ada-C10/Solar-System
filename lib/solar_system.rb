11d3d0c3b04ac03684f3a133eba78c7a85e10490
Madaleine Shields <maddiev.shields@gmail.com>
2018-08-21 23:00:17 -0700

    Begin defining methods in SolarSystem

--- lib/solar_system.rb
+++ lib/solar_system.rb
@@ -1,2 +1,16 @@
 class SolarSystem
+
+  def initialize(star_name)
+    @star_name = star_name
+    @planets = []
+  end
+
+  def add_planet(planet)
+    @planets << planet
+  end
+
+  def list_planets
+    # return list of planets
+  end
+
 end
