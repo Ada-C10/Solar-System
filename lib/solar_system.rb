9a69a45d6e96b69e31cdb021d3d372210d110309
Anibel America <ameriabel@gmail.com>
2018-08-22 11:43:50 -0700

    Adds whitespace and comments to solar system class

--- lib/solar_system.rb
+++ lib/solar_system.rb
@@ -16,6 +16,8 @@ class SolarSystem
     @planets << planet
   end
 
+  # returns a string listing the planets
+  # orbiting the instance of solar system's star_name instance variable
   def list_planets
     list_of_planets = "Planets orbiting #{@star_name}"
 
@@ -26,18 +28,24 @@ class SolarSystem
     return list_of_planets
   end
 
+  # returns a result depending if name argument matches any planets in this solar system
   def find_planet_by_name(planet_name)
 
+    # finds all planets whose planet.name match the the argument
     planets_found = @planets.select do |planet|
       if planet.name.downcase == planet_name.downcase
         planet
       end
     end
 
-    if planets_found.empty?
-      return ERROR_MSG_PLANET_NOT_FOUND
-    elsif planets_found.length == 1
+    # returns the address of the planet if 1 planet is found
+    if planets_found.length == 1
       return planets_found[0]
+    # returns an error message if the planet name was not found
+    elsif planets_found.empty?
+      return ERROR_MSG_PLANET_NOT_FOUND
+    # returns an error message with a list of all all corresponding addresses in memory if
+    # multiple planets with the argument planet_name were found
     else
       planet_findings = ERROR_MSG_MULTIPLE_PLANETS_FOUND
 
@@ -46,16 +54,21 @@ class SolarSystem
       end
       return planet_findings
     end
+
   end
 
+  # returns the distance between 2 planets
   def distance_between(planet_a, planet_b)
+
     planet_a_distance = find_planet_by_name(planet_a).distance_from_sun_km
     planet_b_distance = find_planet_by_name(planet_b).distance_from_sun_km
+
     if planet_a_distance > planet_b_distance
       return planet_a_distance - planet_b_distance
     else
       return planet_b_distance - planet_a_distance
     end
+
   end
 
 end
