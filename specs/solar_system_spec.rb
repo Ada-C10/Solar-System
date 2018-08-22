06b59e5c46dc4086b7f3bf6c75cdc4bf26e10d8d
Madaleine Shields <maddiev.shields@gmail.com>
2018-08-22 12:19:10 -0700

    Write passing tests for solar_system

--- specs/solar_system_spec.rb
+++ specs/solar_system_spec.rb
@@ -3,26 +3,50 @@ require 'minitest/reporters'
 require 'minitest/skip_dsl'
 
 require_relative '../lib/solar_system'
+require_relative '../lib/planet'
 
-require 'pry'
-
-# Get that nice colorized output
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 describe 'SolarSystem' do
   describe 'initialize method' do
     it 'initializes an instance of a Solar System with 1 param' do
 
+      solar_system = SolarSystem.new('Sol')
+
+      solar_system.must_be_instance_of SolarSystem
+
     end
   end
 
   describe 'add_planet method' do
     it 'adds a planet to the array of planets' do
+      solar_system = SolarSystem.new('Sol')
+
+      earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
+
+      solar_system.add_planet(earth)
+
+      solar_system.planets.must_be_kind_of Array
+
+      solar_system.planets.first.name.must_equal "Earth"
     end
   end
 
-  describe ' list_planets method' do
-    'it lists the array of planets' do
+    describe 'list_planets method' do
+      it 'lists the array of planets' do
+        solar_system = SolarSystem.new('Sol')
+
+        earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
+
+        solar_system.add_planet(earth)
+
+        planets_list = solar_system.list_planets
+
+        planets_list.must_be_kind_of Array
+
+        planets_list.first.must_equal "1: Earth"
+      end
     end
-  end
+
+
 end
