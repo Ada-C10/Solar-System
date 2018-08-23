2b4407d85404a10a1adc82a09f538302f746e9d4
Karis Kim <ouniiq@hotmail.com>
2018-08-22 21:26:21 -0700

    incorporated code to allow user to interact with classes. did not add control loop yet

--- main.rb
+++ main.rb
@@ -2,26 +2,42 @@ require_relative 'planet'
 require_relative 'solar_system'
 
 def main
-  earth = Planet.new("Earth", "blue-green", 5.972e24, 1.496e8, "It\'s the only planet known to support life\n")
-  puts earth.name
-  puts earth.color
-  puts earth.summary
+  puts "Fill in the information below to create a planet within your solar system."
+  print "Planet's name: "
+  planet_name = gets.chomp.to_s
+
+  print "Planet's color: "
+  color_of_planet = gets.chomp.to_s
+
+  print "Planet's weight: "
+  weight_of_planet = gets.chomp.to_i
+
+  print "Planet's distance from sun (km): "
+  distance_from_sun = gets.chomp.to_i
+
+  print "A fun fact about the planet: "
+  fun_fact_about_planet = gets.chomp.to_s
+
+  new_planet = Planet.new(planet_name, color_of_planet, weight_of_planet, distance_from_sun, fun_fact_about_planet)
+  puts new_planet.name
+  puts new_planet.color
+  puts new_planet.summary
 
   puts
 
-  venus = Planet.new("Venus", "yellowish white", 4.867e24, 6.724e7, "It\'s the second brightest object in the night sky after the Moon\n")
-  puts venus.name
-  puts venus.color
+  print "Name your solar system: "
+  solary_system_name = gets.chomp.to_s
+  solary_system_name = SolarSystem.new(solary_system_name)
 
-  sun = SolarSystem.new("Sol")
-  sun.add_planet(earth)
-  sun.add_planet(venus)
-
-  list = sun.list_planets
+  solary_system_name.add_planet(new_planet)
+  list = solary_system_name.list_planets
   puts list
 
   puts
-  found_planet_name = sun.find_planet_by_name("Earth") #input: string, #output: string
+  print "Enter the planet you want to like to look up: "
+  planet_to_lookup = gets.chomp.to_s
+
+  found_planet_name = solary_system_name.find_planet_by_name(planet_to_lookup) #input: string, #output: string
   puts found_planet_name
   puts found_planet_name.summary
 end
