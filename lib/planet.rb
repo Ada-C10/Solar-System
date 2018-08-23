88442d8dad5bb13a47cb72716e46cd541243e87f
Kat Perreira <kat.perreira@gmail.com>
2018-08-22 19:17:30 -0700

    Update planet.rb

--- lib/planet.rb
+++ lib/planet.rb
@@ -11,13 +11,7 @@ class Planet
   end
 
   def summary
-    # planet_hash =
-    # { planet_name: @name,
-    #   planet_color: @color,
-    #   planet_mass: @mass_kg,
-    #   planet_distance_from_sun: @distance_from_sun_km,
-    #   planet_fun_fact: @fun_fact
-    # }
+    
     better_formatting =  "The planet is named #{@name}, it is the color #{@color}, it has a mass of #{@mass_kg}, it is #{@distance_from_sun_km} km from the sun and a fun fact about this planet is #{@fun_fact}"
     return better_formatting
   end
