3138db7d4d5715044a44cab0a248ef459cacd070
Hannah <hannahvanseeters@gmail.com>
2018-08-22 21:32:29 -0700

    spec filed added for star_name.rb, but not complete

---
+++ star_name_spec.rb
@@ -0,0 +1,6 @@
+require_relative 'planet'
+require_relative 'star_name'
+require 'minitest/spec' #diff syntax for writing tests
+require 'minitest/autorun'
+require 'minitest/reporters' #nicer display for outcome
+require 'minitest/pride'
