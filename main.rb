27f201a3f62be60cc9834d904a8ba44f63f70185
Kay <kayxn23@gmail.com>
2018-08-22 17:33:45 -0700

    Modifed main.rb to make it more dry, created methods for user options

--- main.rb
+++ main.rb
@@ -1,6 +1,41 @@
 require_relative 'planet'
 require_relative 'solar_system'
 
+
+def ask_and_print_planet_details
+  puts "Which planet would you like to learn about?"
+  user_choice_planet = gets.chomp!
+  if solar_system.planets.include?(user_choice_planet)
+    found_planet = solar_system.find_planet_by_name(user_choice_planet)
+    puts found_planet.summary
+  else
+    puts "ERROR: This planet does not exist yet in #{solar_system.star_name}, choose 'list planets' to see the existing planets"
+  end
+end
+
+def ask_and_add_planet_to_list(solar_system)
+  puts "What is the planet name?"
+  name = gets.chomp!
+  puts "What is the color of #{name}?"
+  color = gets.chomp!
+  puts "What is the mass of #{name}?"
+  mass = gets.chomp.to_f
+  until mass > 0
+    puts "ERROR: The mass must be greater than 0, what is the mass of #{name}?"
+    mass = gets.chomp.to_f
+  end
+  puts "What is the distance of #{name} from the sun?"
+  distance = gets.chomp.to_f
+  until distance > 0
+    puts "ERROR: The distance must be greater than 0, what is the distance of #{name}?"
+    distance = gets.chomp.to_f
+  end
+  puts "What is a fun fact about #{name}?"
+  fun_fact = gets.chomp!
+  user_planet = Planet.new(name, color, mass, distance, fun_fact)
+  solar_system.add_planet(user_planet)
+end
+
 def main
   #create solar system
   solar_system = SolarSystem.new("Sol")
@@ -13,46 +48,51 @@ def main
   solar_system.add_planet(saturn)
   solar_system.add_planet(jupiter)
   # create loop that repeatedly asks user what to do next
-  puts "What do you want to do next? choose one: list planets, exit, planet details, add planet"
-  action = gets.chomp!
+  options = ["exit", "list planets", "planet details", "add planet"]
+  # options.each do |option| puts "#{option}" end
+  puts "Welcome"
+  action = ""
+  # until options.include?(action) puts "ERROR: What do you want to do next? choose one: #{options}"
   until action == "exit"
-    if action == "list planets"
+    puts "What do you want to do next? choose one: #{options}"
+    action = gets.chomp!
+    case action
+    when "list planets"
       puts list_of_planets = solar_system.list_planets
-      puts "What do you want to do next? choose one: list planets, exit, planet details, add planet"
-      action = gets.chomp!
-    elsif action == "planet details"
-      puts "Which planet would you like to learn about?"
-      user_choice_planet = gets.chomp!
-      found_planet = solar_system.find_planet_by_name(user_choice_planet)
-      puts found_planet.summary
-      puts "What do you want to do next? choose one: list planets, exit, planet details, add planet"
-      action = gets.chomp!
-    elsif action == "add planet"
-      puts "What is the planet name?"
-      name = gets.chomp!
-      puts "What is the color of #{name}?"
-      color = gets.chomp!
-      puts "What is the mass of #{name}?"
-      mass = gets.chomp.to_f
-      until mass > 0
-        puts "ERROR: The mass must be greater than 0, what is the mass of #{name}?"
-        mass = gets.chomp.to_f
-      end
-      puts "What is the distance of #{name} from the sun?"
-      distance = gets.chomp.to_f
-      until distance > 0
-        puts "ERROR: The distance must be greater than 0, what is the distance of #{name}?"
-        distance = gets.chomp.to_f
-      end
-      puts "What is a fun fact about #{name}?"
-      fun_fact = gets.chomp!
-      user_planet = Planet.new(name, color, mass, distance, fun_fact)
-      solar_system.add_planet(user_planet)
-      puts "What do you want to do next? choose one: list planets, exit, planet details, add planet"
-      action = gets.chomp!
+    when "planet details"
+      ask_and_print_planet_details
+      # puts "Which planet would you like to learn about?"
+      # user_choice_planet = gets.chomp!
+      # if solar_system.planets.include?(user_choice_planet)
+      #   found_planet = solar_system.find_planet_by_name(user_choice_planet)
+      #   puts found_planet.summary
+      # else
+      #   puts "ERROR: This planet does not exist yet in #{solar_system.star_name}, choose 'list planets' to see the existing planets"
+      # end
+    when "add planet"
+      ask_and_add_planet_to_list(solar_system)
+      # puts "What is the planet name?"
+      # name = gets.chomp!
+      # puts "What is the color of #{name}?"
+      # color = gets.chomp!
+      # puts "What is the mass of #{name}?"
+      # mass = gets.chomp.to_f
+      # until mass > 0
+      #   puts "ERROR: The mass must be greater than 0, what is the mass of #{name}?"
+      #   mass = gets.chomp.to_f
+      # end
+      # puts "What is the distance of #{name} from the sun?"
+      # distance = gets.chomp.to_f
+      # until distance > 0
+      #   puts "ERROR: The distance must be greater than 0, what is the distance of #{name}?"
+      #   distance = gets.chomp.to_f
+      # end
+      # puts "What is a fun fact about #{name}?"
+      # fun_fact = gets.chomp!
+      # user_planet = Planet.new(name, color, mass, distance, fun_fact)
+      # solar_system.add_planet(user_planet)
     end
   end
-  list_of_planets = solar_system.list_planets
 end
   # puts list_of_planets = solar_system.list_planets
   #
