601e947a1ea3928793efe759b8841f7e68613858
Madaleine Shields <maddiev.shields@gmail.com>
2018-08-22 22:07:40 -0700

    Write planet details loop to display info

--- lib/main.rb
+++ lib/main.rb
@@ -1,14 +1,45 @@
 require_relative 'planet'
 require_relative 'solar_system'
 
+require 'pry'
+
 def main
-  solar_system = SolarSystem.new('Sol')
+  @solar_system = SolarSystem.new('Sol')
 
   earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
-  solar_system.add_planet(earth)
 
-  list = solar_system.list_planets
+  jupiter = Planet.new('Jupiter', 'red', 1.898e27, 7.78e6, 'That red eye tho')
+
+  make_believe = Planet.new('Make Believe', 'turquoise', 3.2e25, 4.21e7, 'I just made this planet up!')
+
+  @solar_system.add_planet(earth)
+  @solar_system.add_planet(jupiter)
+  @solar_system.add_planet(make_believe)
+
+  planet_details
+
+end
+
+def planet_details
+  list = @solar_system.list_planets
+
+  puts "What planet would you like to learn about?"
   puts list
+  puts "\nPlease type in a planet"
+  user_input = gets.chomp.downcase
+
+  options = list.map do |element|
+    element.split.last.downcase
+  end
+
+  until options.include?(user_input)
+    puts "\nInvalid entry. Try again.\n"
+    user_input = gets.chomp.downcase
+  end
+
+  found_planet = @solar_system.find_planet_by_name(user_input)
+  puts found_planet.summary
+
 end
 
 main
