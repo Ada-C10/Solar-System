8c2ffc50e2a0f73f40efab0160dcf29253e5f69e
Layla <layla.bristol@gmail.com>
2018-08-22 21:23:37 -0700

    wave 2 completed

--- main.rb
+++ main.rb
@@ -1,16 +1,25 @@
 require_relative 'planet'
+require_relative 'solar_system'
 
+#### wave 1
 def main
  earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
 
- puts earth.name.upcase
- puts earth.summary
 
 mars = Planet.new('Mars', 'red', 6.39e23, 1.524e8, 'Named after the Roman god of war')
 
- puts mars.name.upcase
- puts mars.summary
+#### wave 2
 
+solar_system = SolarSystem.new('Sol')
+solar_system.add_planet(earth)
+solar_system.add_planet(mars)
+
+list = solar_system.list_planets
+puts list
+
+found_planet = solar_system.find_planet_by_name('Earth')
+puts found_planet
+puts found_planet.summary
 
 end
 
