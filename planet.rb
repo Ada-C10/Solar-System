e173aea421d65f82373eac08a76c3cffd8cd55e6
sabine <drsabinethomas@gmail.com>
2018-08-23 08:38:29 -0700

    Wave 3 completed with some bugs

--- planet.rb
+++ planet.rb
@@ -1,9 +1,10 @@
-# This the class Planet
+# This the class Planet to track information
+# about a single planet
 class Planet
 
 # The helper method (attr_reader) to make variables readable outside the class
 attr_reader :name, :color, :mass_kg, :distance_from_sun_km, :fun_fact
-# Here I added constructor with 5 parameters saved in an instance variable
+# Add constructor with 5 parameters saved in an instance variable
 # of the same name
   def initialize (name, color, mass_kg, distance_from_sun_km, fun_fact)
     @name = name
@@ -13,6 +14,8 @@ attr_reader :name, :color, :mass_kg, :distance_from_sun_km, :fun_fact
     @fun_fact = fun_fact
   end
 
+# Add an instance method to Planet called summary.
+# This method returns a string of a formatted description of the planet.
   def summary
      return "#{@name} whose color is #{@color}, weighs #{@mass_kg}, and is #{@distance_from_sun_km} from the sun. One of its fun facts is that it #{@fun_fact}."
   end
--- solar_system.rb
+++ solar_system.rb
@@ -1,6 +1,13 @@
 require_relative 'planet'
 
+# Building a SolarSystem class
+# which is responsible for keeping track of a collection of instances of Planet.
 class SolarSystem
+
+# star_name parameter is saved as an instance variable.
+# also an instance variable called @planets, to store an array of planets.
+# @planets is set as an empty array.
+# Both @star_name and @planets are readable but not writable.
 
   attr_reader :star_name,:planets
   def initialize(star_name)
@@ -8,11 +15,16 @@ class SolarSystem
     @planets = []
   end
 
+# New method which will take an instance of Planet
+# as a parameter and add it to the list of planets.
   def add_planet(planet)
     @planets << planet
     return @planets
   end
 
+ # New method which will return
+ # a string containing a list of all the planets
+ # in the system.
   def list_planets
     planet_list = "Planets orbiting #{@star_name} "
     i = 1
@@ -23,14 +35,19 @@ class SolarSystem
     return planet_list
   end
 
+# New method that takes the name of a planet as a string,
+# and returns the corresponding instance of Planet.
+# The lookup should be case-insensitive, so that Earth, earth and eArTh all return the same planet.
+
   def find_planet_by_name(planet_string)
     @planets.each do |planet|
       if planet.name.downcase == planet_string.downcase
         return planet
       end
 
+      return "This planet doesn't exist"
+      
     end
-        return "This planet doesn't exist"
 
   end
 
