8e8bcc4ac66e95a9c1959c2666023b449e9fea3b
Kat Perreira <kat.perreira@gmail.com>
2018-08-22 18:45:52 -0700

    Restructured to have better summary sentence

--- lib/planet.rb
+++ lib/planet.rb
@@ -1,6 +1,6 @@
 class Planet
 
-  attr_reader :name, :color, :mass_kg, :distance_from_sun_km, :fun_fact
+  attr_accessor :name, :color, :mass_kg, :distance_from_sun_km, :fun_fact
 
   def initialize (name, color, mass_kg, distance_from_sun_km, fun_fact)
     @name = name
@@ -11,17 +11,15 @@ class Planet
   end
 
   def summary
-    planet_hash = [
-      { planet_name: @name,
-        planet_color: @color,
-        planet_mass: @mass_kg,
-        planet_distance_from_sun: @distance_from_sun_km,
-        planet_fun_fact: @fun_fact
-      }
-    ]
-
-
-    return planet_hash
+    planet_hash =
+    { planet_name: @name,
+      planet_color: @color,
+      planet_mass: @mass_kg,
+      planet_distance_from_sun: @distance_from_sun_km,
+      planet_fun_fact: @fun_fact
+    }
+    better_formatting =  "The planet is named #{@name}, it is the color #{@color}, it has a mass of #{@mass_kg}, it is #{@distance_from_sun_km} km from the sun and a fun fact about this planet is #{@fun_fact}"
+    return better_formatting
   end
 
 end
